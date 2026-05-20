9567b9b3f6d86caa01510084a8299b0374950d38
Alyson <alysonlucn@gmail.com>
2026-05-20 17:22:21 -0300

    feat: initialize setup on project

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import app  from './app';
+import dotenv from 'dotenv';
+import { AppDataSource } from './config/database';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error);
+  });
